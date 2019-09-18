f7a70578ef189af54781cfd6cf11ca0ea2b3247a
Paul Schlueter <paul@paulschlueter.com>
2019-09-18 10:38:13 -0500

    Specified R Version and Package Versions (#5)
    Identified specific versions of R and R packages in the docker image definition.

---
+++ container/install-packages.R
@@ -0,0 +1,8 @@
+install.packages('devtools')
+library(devtools)
+
+install_version('mda', '0.4-10')
+install_version('plumber', '0.4.6')
+install_version('tidyverse', '1.2.1')
+install_version('caret', '6.0-84')
+install_version('forecast', '8.9')
